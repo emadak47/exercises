d51e8c56042a7e366f12a592f7eb9faf4c814244
Emad Akhras <emadakhrasidleb@gmail.com>
2026-04-17 16:23:44 +0800

    What is a parser?

---
+++ build-your-own-x/parser-combinators/src/lib.rs
@@ -0,0 +1,65 @@
+#[derive(Debug)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
+fn match_literal<'a, 'b>(expected: &'a str) -> impl Fn(&'b str) -> Result<(&'b str, ()), &'b str> {
+    move |input| match input.split_at_checked(expected.len()) {
+        Some((first, last)) if first == expected => Ok((last, ())),
+        _ => Err(input),
+    }
+}
+
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(ch) if ch.is_alphabetic() => matched.push(ch),
+        _ => return Err(input),
+    }
+
+    for ch in chars {
+        if ch.is_alphanumeric() || ch == '-' {
+            matched.push(ch);
+        } else {
+            break;
+        }
+    }
+
+    Ok((&input[matched.len()..], matched))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_literal() {
+        let parse_joe = match_literal("Hello Joe!");
+        assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+        assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+
+    #[test]
+    fn test_identifier() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
+    }
+}
